b9953a3731a09d821b6226746ff3db6afcaeef68
Vinay Phatarpekar <vinayp9399@gmail.com>
2024-06-26 19:22:39 +0530

    1234

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 8080;
+// const port = 8080;
 app.use(cors());
 app.use(express.json({origin:"http://e-comwebsite-frontend.vercel.app"}));
 app.use('/users',require('./routes/useroutes'))
 app.use('/products',require('./routes/productroutes'))
 app.use('/wishlist',require('./routes/wishlistroutes'))
 app.use('/cart',require('./routes/cartroutes'))
-app.listen(port,(error)=>{
-    console.log(`server has started at ${port}`)
-})
+// app.listen(port,(error)=>{
+//     console.log(`server has started at ${port}`)
+// })
