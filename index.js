7ec01972671c540a7cc14be69ae8d61ac255d5e5
Vinay Phatarpekar <vinayp9399@gmail.com>
2024-06-26 20:15:26 +0530

    1234

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.use('/users',require('./routes/useroutes'))
 app.use('/products',require('./routes/productroutes'))
 app.use('/wishlist',require('./routes/wishlistroutes'))
 app.use('/cart',require('./routes/cartroutes'))
+
+app.get("/", (req,res)=>{
+   res.status(200).send("Server started");
+});
+
 app.listen(port,(error)=>{
     console.log(`server has started at ${port}`)
 })
