8f99f3a18509709286966196dc68ed9a9cdab088
Vinay Phatarpekar <vinayp9399@gmail.com>
2024-06-26 16:39:45 +0530

    1234

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 const port = 8080;
 app.use(cors());
-app.use(express.json({origin:"http://localhost:3000"}));
+app.use(express.json({origin:"http://e-comwebsite-frontend.vercel.app"}));
 app.use('/users',require('./routes/useroutes'))
 app.use('/products',require('./routes/productroutes'))
 app.use('/wishlist',require('./routes/wishlistroutes'))
