680698ad3b23b8a5ffb883519bb557628e4ca78a
Vinay Phatarpekar <vinayp9399@gmail.com>
2024-07-01 15:58:36 +0530

    1234

--- config/mongodb_connect.js
+++ config/mongodb_connect.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/newkart?directConnection=true&serverSelectionTimeoutMS=10000&appName=mongosh+2.0.2",{useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+mongoose.connect("mongodb://127.0.0.1:3306/newkart?directConnection=true&serverSelectionTimeoutMS=10000&appName=mongosh+2.0.2",{useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
     console.log("Connected to Database");
     }).catch((err) => {
         console.log("Not Connected to Database ERROR! ", err);
