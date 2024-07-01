9976554e2644a156580f83a1d28e6b81364f9c65
Vinay Phatarpekar <vinayp9399@gmail.com>
2024-07-01 10:25:34 +0530

    1234

--- config/mongodb_connect.js
+++ config/mongodb_connect.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/newkart",{});
+mongoose.connect("mongodb://localhost:27017/newkart?directConnection=true&server",{});
 const usersSchema = {
     firstname:String,
     lastname:String,
